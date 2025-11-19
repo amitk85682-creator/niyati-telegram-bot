83ddffe3c3eb2a3e8d75956eb4f2285d470c9bd0
amitk85682-creator <amitk85682@gmail.com>
2025-11-19 09:37:06 +0530

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # --- config.py ---
 
 # Replace with the token you get from BotFather
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+TELEGRAM_TOKEN = "7833989616:AAEopVatvSTIrIWsiqoaMjgZ4URnKeHwUSM"
 
 # Replace with the numerical Telegram User IDs of the admins
 # You can get your ID by messaging @userinfobot
-ADMIN_IDS = {123456789, 987654321}
+ADMIN_IDS = {6946322342}
 
 # A secret PIN for the /broadcast command to prevent misuse
-BROADCAST_PIN = "NIYATI_PIN_123"
+BROADCAST_PIN = "niyati_secret_2025"
 
 # Database file name
 DB_NAME = "niyati_bot.db"
